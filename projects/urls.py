4b86a608a4da523098135af9b6c1e560d197c363
jaimemaureira01 <jaime_maureira_a@hotmail.es>
2025-05-02 04:17:39 -0400

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -3,7 +3,7 @@ from .api import PersonaViewSet, ClienteViewSet, AdministradorViewSet
 
 router = routers.DefaultRouter()
 
-router.register('persona', PersonaViewSet, 'persona')
+router.register('api/persona', PersonaViewSet, 'persona')
 router.register('api/cliente', ClienteViewSet, 'cliente')
 router.register('api/administrador', AdministradorViewSet, 'administrador')
     
