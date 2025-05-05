3c8cda2086915d39f8373fcd99023396898083ab
jaimemaureira01 <jaime_maureira_a@hotmail.es>
2025-05-05 17:37:39 -0400

    Update views.py

--- projects/views.py
+++ projects/views.py
@@ -9,11 +9,11 @@ def post_api_register (post_data):
     
     URL_API = "http://127.0.0.1:8000/api/persona/"
     try:
-        response = requests.post(URL_API, json=post_data)
-        if response.status_code == 201:
-            return response.json()
+        post = requests.post(URL_API, json=post_data)
+        if post.status_code == 201:
+            return post.json()
         else:
-            return {"error": f"Error al realizar la solicitud: {response.status_code}", "detalles": response.text}
+            return {"error": f"Error al realizar la solicitud: {post.status_code}", "detalles": post.text}
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         return {"error": "Error de conexión con la API", "details": str(e)}
