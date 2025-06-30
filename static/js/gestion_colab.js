2da43fc90c150795725e0e169dc1221fc76c61e5
jaimemaureira01 <135204632+jaimemaureira@users.noreply.github.com>
2025-06-29 23:13:49 -0400

    gestion
    se crea meni gestion colaboradores

--- static/js/gestion_colab.js
+++ static/js/gestion_colab.js
@@ -1,5 +1,33 @@
 
 // funcion para hacer el boton burger
-function toggleSidebar() {
-  document.getElementById('sidebar').classList.toggle('show');
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.getElementById('menuToggle');
+  const sidebar = document.getElementById('sidebar');
+  const mainContent = document.getElementById('main-content');
+
+  // Botón para abrir/cerrar
+  menuToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('show');
+    mainContent.classList.toggle('shifted');
+  });
+
+  // Cerrar al hacer clic fuera
+  document.addEventListener('click', (event) => {
+    // Si no está abierto, no hacer nada
+    if (!sidebar.classList.contains('show')) return;
+
+    // Si clic en botón o sidebar, no cerrar
+    if (sidebar.contains(event.target) || menuToggle.contains(event.target)) return;
+
+    // Clic afuera -> cerrar
+    sidebar.classList.remove('show');
+    mainContent.classList.remove('shifted');
+    
+  });
+  // sidebar.querySelectorAll('a').forEach(link => {
+  //   link.addEventListener('click', () => {
+  //     sidebar.classList.remove('show');
+  //     mainContent.classList.remove('shifted');
+  //   });
+  // });
+});
